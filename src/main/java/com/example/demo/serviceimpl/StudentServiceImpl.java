c9691c38e0b25f87ca99f6a544d335b07775ca75
bala-amypo <bala@amypo.in>
2025-12-17 09:24:29 +0000

    User commit

---
+++ src/main/java/com/example/demo/serviceimpl/StudentServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.demo.serviceimpl;
+
+import com.example.demo.service.service;
+
+import com.example.demo.service.Stuservice;
+
+@service
+public class Stuserviceimpl implements Stuservice{
+    
+}
